1dbba1347131b0116fab0671b66e94e52f8c3e7b
林京秦 <hzljq@tairanchina.com>
2016-08-24 10:27:56 +0800

    senderPhone = userInfo.phone

--- js/controllers/sms.js
+++ js/controllers/sms.js
@@ -1,5 +1,5 @@
-angular.module('MetronicApp').controller('smsController', ['$scope', '$rootScope', '$timeout', 'ModalService', 'smsAPI', 'platformAPI', 'templateAPI',
-	function($scope, $rootScope, $timeout ,ModalService, smsAPI, platformAPI, templateAPI) {
+angular.module('MetronicApp').controller('smsController', ['$scope', '$rootScope', '$timeout', 'ModalService', 'MyAuthService', 'smsAPI', 'platformAPI', 'templateAPI',
+	function($scope, $rootScope, $timeout ,ModalService, MyAuthService, smsAPI, platformAPI, templateAPI) {
     
     platformAPI.all({}, function(data) {
         $scope.platformC = data.infos;
@@ -14,11 +14,14 @@ angular.module('MetronicApp').controller('smsController', ['$scope', '$rootScope
     });
 
     $scope.post = function(formData) {
+        var userInfo = MyAuthService.getUserInfo();
+        formData.senderPhone = userInfo.phone;
     	smsAPI.send({platformCode: formData.platformCode}, $.param(formData), function(data) {
-    		console.log(data);
+    		
     		toastr.success('发送短信成功');
     	}, function(err) {
     		toastr.error(err.data.error.description);
     	})
     }
+
 }]);
