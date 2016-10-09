8bbe2c21eacd496c5fa82ca6dacc7124b4861a53
贾振山 <hzjzs@tairanchina.com>
2016-10-09 22:12:14 +0800

    js

---
+++ js/service/messageallAPI.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Administrator on 2016/10/2.
+ */
+angular.module('MetronicApp').factory('messageallAPI', ["$resource","$rootScope", function ($resource,$rootScope) {
+    return $resource(messageAllURL, {}, {
+        getmessageall: {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Cache-Control" : "no-cache"
+            },
+            url: messageAllURL+"/messageinfo/list"
+        },
+        getregion: {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Cache-Control" : "no-cache"
+            },
+            url: messageAllURL+"/popup/brokertag"
+        },
+        getapp: {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Cache-Control" : "no-cache"
+            },
+            url: messageAllURL+"/popup/app"
+        }
+    })
+}]);
